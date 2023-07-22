5540a736ef8c67920a41df57bef26447f7bd5914
ajith30 <ajithkumark30@gmail.com>
2023-07-23 03:57:41 +0530

    testing

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -6,14 +6,18 @@ const generateToken = (res, userId) => {
     expiresIn: "1h"
   });
 
+console.log(process.env.NODE_ENV);
+
   //Set JWT as an HTTP-Only cookie (storing the token in cookie insted of localStorage)
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development", //secure cookie in production environment
+    secure: process.env.NODE_ENV === "production", //secure cookie in production environment
     sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // Allow the cookie to be sent with cross-site requests only in production
     //sameSite: "true",//"strict",  //Prevent CSRF attacks
     maxAge: 60 * 60 * 1000,  // 1 hour in milliseconds
   });
 }
+
+
 
 export default generateToken;
