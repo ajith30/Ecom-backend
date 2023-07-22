a67593935e03b79f07c7b1f26d94b4e5e142a54d
ajith30 <ajithkumark30@gmail.com>
2023-07-22 08:59:35 +0530

    new changes

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -10,7 +10,8 @@ const generateToken = (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development", //secure cookie in production environment
-    sameSite: "true",//"strict",  //Prevent CSRF attacks
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // Allow the cookie to be sent with cross-site requests only in production
+    //sameSite: "true",//"strict",  //Prevent CSRF attacks
     maxAge: 60 * 60 * 1000,  // 1 hour in milliseconds
   });
 }
